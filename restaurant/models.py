416cb8f011ac3365a559242f8f6e5f0d35c21e1a
El hadji sow <sowelhaz@gmail.com>
2025-04-14 02:33:17 +0200

    add tests/

--- restaurant/models.py
+++ restaurant/models.py
@@ -1,25 +1,33 @@
 from django.db import models
 
+
+class BaseModel(models.Model):
+    created_at = models.DateTimeField(db_index=True, auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
 # Create your models here.
-class Booking(models.Model):
+class Booking(BaseModel):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=255)
-    date = models.DateTimeField()
+    date = models.DateField()
     time = models.TimeField()
     no_of_guests = models.IntegerField(6)
-    created_at = models.DateTimeField(auto_now_add=True)
+   
 
     def __str__(self):
-        return self.name
+        return f'{self.name} -- {self.no_of_guests} -- {self.date}'
     
-class Menu(models.Model):
+class Menu(BaseModel):
     id = models.AutoField(primary_key=True)
     title = models.CharField(max_length = 255)
     price = models.DecimalField(max_digits = 5, decimal_places = 2)
     inventory = models.IntegerField(default = 0)
-    created_at = models.DateTimeField(auto_now_add = True)
+    
     def __str__(self):
-        return self.title, self.price, self.description, self.inventory
+        return f'{self.title} : {str(self.price)}'
     
 
     
--- restaurant/urls.py
+++ restaurant/urls.py
@@ -5,7 +5,8 @@ from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns =[
     path('', views.index, name='home'),
-    path('menu/', views.MenuItemAPIView.as_view()),
-    path('menu/<int:pk>', views.SingleMenuItemAPIView.as_view()),
+    path('menu/', views.MenuItemAPIView.as_view(), name='menu'),
+    
+    path('menu/<int:pk>', views.SingleMenuItemAPIView.as_view(), name='single_menu_item'),
     path('api-token-auth/', obtain_auth_token, name='api_token_auth'),
 ]
